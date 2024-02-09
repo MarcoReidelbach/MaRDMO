5991d08c4ded23accd0e718988f06b8326d8d777
MarcoReidelbach <reidelbach@zib.de>
2024-02-09 11:12:12 +0100

    Adjusted Configuration and IDs for Staging Portal

--- MaRDMO/config.py
+++ MaRDMO/config.py
@@ -1,12 +1,16 @@
+#Portal Bot Credentials
+lgname = '' 
+lgpassword = ''
+
 #Portal Wiki, Api and SPARQL Endpoint
-mardi_wiki="https://portal.mardi4nfdi.de/wiki/"
-mardi_api="https://portal.mardi4nfdi.de/w/api.php"
-mardi_endpoint="https://query.portal.mardi4nfdi.de/proxy/wdqs/bigdata/namespace/wdq/sparql"
+mardi_wiki="https://staging.mardi4nfdi.org/wiki/"
+mardi_api="https://staging.mardi4nfdi.org/w/api.php"
+mardi_endpoint="https://query.staging.mardi4nfdi.org/proxy/wdqs/bigdata/namespace/wdq/sparql"
 
 #Wikidata SPARQL, Api Endpoint
 wikidata_endpoint="https://query.wikidata.org/sparql"
 wikidata_api="https://www.wikidata.org/w/api.php"
 
 #SPARQL Prefixes
-wd = '<https://portal.mardi4nfdi.de/entity/>'
-wdt = '<https://portal.mardi4nfdi.de/prop/direct/>'
+wd = '<https://staging.mardi4nfdi.org/entity/>'
+wdt = '<https://staging.mardi4nfdi.org/prop/direct/>'
